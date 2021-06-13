e7539f39ce9a63cbf5dea977322d9c4b87fcbd78
Bayron Moors <bayronmoors@gmail.com>
2021-06-13 12:47:20 +0200

    HTML CSS DONE

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('trigger').onclick = function (e) {
+    document.getElementById('triggered').classList.toggle('visible');
+}
+
+document.getElementById('close').onclick = function(e) {
+    document.getElementById('triggered').classList.remove('visible');
+}
